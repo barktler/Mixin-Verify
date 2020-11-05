b6eb50c61a24dcc31eb9fc094b4a41b66886b5c8
WMXPY <wm@mengw.io>
2020-11-05 14:12:38 -0600

    add dpednencies

--- script/license.ts
+++ script/license.ts
@@ -26,6 +26,7 @@ const appPackage: any = {
     license: parent.license,
     bugs: parent.bugs,
     homepage: parent.homepage,
+    dependencies: parent.dependencies,
     peerDependencies: parent.peerDependencies,
 };
 Fs.writeFileSync(Path.join(appPath, 'package.json'), JSON.stringify(appPackage, null, 2), 'utf8');
